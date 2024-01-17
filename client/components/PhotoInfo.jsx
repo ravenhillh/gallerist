9b76cc6a0e69363539ea5c5c9741d4d05fb0eca3
kycodee <ky.patton87@gmail.com>
2024-01-17 15:02:43 -0600

    photoInfo Component rendering data from db to test functionality

--- client/components/PhotoInfo.jsx
+++ client/components/PhotoInfo.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function PhotoInfo() {
+
+  const [currPhoto, changePhoto] = useState([])
+  function getPhotoInfo(){
+    axios('/db/art/')
+    .then((results) => {
+
+      // console.log(results.data[0])
+      changePhoto(results.data[0])
+    })
+    .catch((err) => console.error(err))
+  }
+  getPhotoInfo();
   return (
-    <h1 id="PhotoInfo">Photo Info Here</h1>
+    <div>
+      <h1 id="PhotoInfo">{currPhoto.title}</h1>
+      <ul>
+        <li>Title:{currPhoto.title}</li>
+        <li>Artist:{currPhoto.artist}</li>
+        <li>Date:{currPhoto.date}</li>
+        <li>Culture:{currPhoto.culture}</li>
+        <li>Title Again:{currPhoto.title}</li>
+      </ul>
+    </div>
   );
 }
 
