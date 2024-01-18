52a5e0d5d557d1dd8ac4ba2c67121c58b73ca0ab
jrfiii <rob.frank.iii@gmail.com>
2024-01-18 17:26:20 -0600

    add styling to auction page

--- client/components/AuctionItem.jsx
+++ client/components/AuctionItem.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
 function AuctionItem({ art, setSale }) {
   const [hover, setHover] = useState(false);
   const onHover = () => setHover(true);
@@ -16,20 +20,37 @@ function AuctionItem({ art, setSale }) {
   }
 
   return (
-    <div>
-      {hover ? <div>{art.title}</div> : <div> </div>}
-      <img
-        style={{ width: '250px', height: 'auto' }}
-        src={art.imageUrl}
-        onMouseEnter={onHover}
-        onMouseLeave={onLeave}
-        alt={art.title}
-      />
-      <button onClick={bidClick} value={art.imageId} type="button">
-        Buy
-      </button>
-    </div>
+    <Col>
+      <Card style={{ width: '20rem' }}>
+        <Card.Img
+          variant="top"
+          alt={art.title}
+          // style={{ width: '250px', height: 'auto' }}
+          src={art.imageUrl}
+        />
+        <Card.Body>
+          <Card.Title>{art.title}</Card.Title>
+          <Card.Text>{art.artist}</Card.Text>
+          <Button onClick={bidClick} value={art.imageId} variant="outline-dark">
+            Buy
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
+// {/* <div>
+// {hover ? <div>{art.title}</div> : <div> </div>}
+// <img
+//   style={{ width: '250px', height: 'auto' }}
+//   src={art.imageUrl}
+//   onMouseEnter={onHover}
+//   onMouseLeave={onLeave}
+//   alt={art.title}
+// />
+// <button onClick={bidClick} value={art.imageId} type="button">
+//   Buy
+// </button>
+// </div> */}
 
 export default AuctionItem;
