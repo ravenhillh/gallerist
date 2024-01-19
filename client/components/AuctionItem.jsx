3d9eb980e41ed06fc8c4b606f2ec683d2c690fb8
jrfiii <rob.frank.iii@gmail.com>
2024-01-19 09:44:37 -0600

    add db request to auction to keep items up to date if another client buys while also on auction page

--- client/components/AuctionItem.jsx
+++ client/components/AuctionItem.jsx
@@ -6,9 +6,9 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 
 function AuctionItem({ art, setSale }) {
-  const [hover, setHover] = useState(false);
-  const onHover = () => setHover(true);
-  const onLeave = () => setHover(false);
+  // const [hover, setHover] = useState(false);
+  // const onHover = () => setHover(true);
+  // const onLeave = () => setHover(false);
 
   function bidClick(event) {
     axios
