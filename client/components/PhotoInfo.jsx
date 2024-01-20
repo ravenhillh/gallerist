65bd9d64e5789b3984e235d9bcb670c0413605b7
student <student@Carins-MacBook-Pro.local>
2024-01-19 18:37:59 -0600

    photoInfo harvard site link/add Friend/styling

--- client/components/PhotoInfo.jsx
+++ client/components/PhotoInfo.jsx
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
 
 function PhotoInfo() {
   const { imageId } = useParams();
   const [currPhoto, changePhoto] = useState([]);
-
+  const [photoOwner, befriendOwner] = useState('');
   function getPhotoInfo() {
     axios(`/db/artwork/${imageId}`)
       .then((results) => {
+        // console.log(results.data[0].userGallery.name)
         changePhoto(results.data[0]);
+        befriendOwner(results.data[0].userGallery.name)
       })
       .catch((err) => console.error(err));
   }
+
+  function addFriend(e) {
+    // console.log(e.target.value)
+    axios.put('/db/friends/', { friend: e.target.value })
+      .then(() => console.log('added friend!', e.target.value))
+      .catch((err) => console.log(err, 'friend not added'));
+  } 
 
   useEffect(() => {
     getPhotoInfo();
@@ -23,7 +34,9 @@ function PhotoInfo() {
   return (
     <div>
       <Image fluid alt={currPhoto.title} src={currPhoto.imageUrl} />
-      <h1 id="PhotoInfo">{currPhoto.title}</h1>
+      <Container style={{display: 'flex', justifyContent: 'center'}}>
+        
+      <h1 id="PhotoInfo" >{currPhoto.title}</h1>
       <ul>
         <li>
           Title:
@@ -48,7 +61,9 @@ function PhotoInfo() {
         <li>
           <a href={currPhoto.url}>Click for More Info</a>
         </li>
+          <Button variant="secondary" value={photoOwner} onClick={addFriend}>Add Friend</Button>
       </ul>
+      </Container>
     </div>
   );
 }
