d1bb2e172b8afa3c2029ff7c34456fd5d41a904f
ravenhillh <ravenhillh@gmail.com>
2024-05-10 16:39:01 -0500

    fix mongo atlas

--- server/db/index.js
+++ server/db/index.js
@@ -5,12 +5,13 @@ const findOrCreate = require('mongoose-findorcreate');
 const { Schema, model } = mongoose;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const { ATLAS_URI } = process.env;
 
-const db_uri = 'mongodb://localhost/gallerist';
+const uri = process.env.MONGODB_URI;
+
+// const db_uri = 'mongodb://localhost/gallerist';
 
 mongoose
-  .connect(ATLAS_URI)
+  .connect(uri)
   .then(() => console.log('Connection to Database successful'))
   .catch((err) => console.log('Could not connect to database ', err));
 
